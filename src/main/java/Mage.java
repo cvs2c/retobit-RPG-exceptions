91cad465da4bbf516166938d268130b01275ac02
eduardo <cuevaseduardo0623@gmail.com>
2026-02-03 13:32:51 -0300

    Realizado

--- src/main/java/Mage.java
+++ src/main/java/Mage.java
@@ -7,20 +7,47 @@ public class Mage extends Character {
         this.mana = mana;
     }
 
+    public static void checkMana(int manaCost, int currentMana, String mageName) throws InsufficientManaException{
+        if(manaCost > currentMana){
+            throw new InsufficientManaException( mageName + " no tiene maná suficiente");
+        }
+    }
+
+    public static void checkTargetNotNull(Character target) throws InvalidTargetException{
+        if(target == null){
+            throw new InvalidTargetException("No se puede realizar una acción a un objetivo nulo");
+        }
+    }
+
+    public static void checkIsAlive(Character target) throws CharacterAlreadyDeadException{
+        if(!target.isAlive()){
+            throw new CharacterAlreadyDeadException( target.name + " ya está muerto");
+        }
+    }
+
     public int getMana() {
         return this.mana;
     }
 
     public void castSpell(Character target) {
 
+        checkTargetNotNull(target);
+        checkIsAlive(target);
+
         if (this.mana - 10 >= 0) {
             this.mana -= 10;
             target.receiveDamage(20);
             target.status = Status.POISONED;
         }
+
+        checkMana(10, this.mana, this.name);
+
     }
 
     public void heal(Character target) {
+            checkMana(15, this.mana, this.name);
+            checkTargetNotNull(target);
+            checkIsAlive(target);
             this.mana -= 15;
             target.health += 20;
     }
--- src/main/java/Warrior.java
+++ src/main/java/Warrior.java
@@ -1,3 +1,5 @@
+import java.lang.annotation.Target;
+
 public class Warrior extends Character {
     int strength;
 
@@ -6,16 +8,37 @@ public class Warrior extends Character {
         this.strength = strength;
     }
 
+    /*Validaciones*/
+    public static void checkBerserkMode(Status status, String warrior) throws BerserkAlreadyActiveException{
+        if(status == Status.FURIOUS){
+            throw new BerserkAlreadyActiveException(warrior + " ya está en modo furioso");
+        }
+    }
+
+    public static void checkTargetNotNull(Character target) throws InvalidTargetException{
+        if(target == null){
+            throw new InvalidTargetException("No se puede realizar una acción a un objetivo nulo");
+        }
+    }
+
+    public static void checkIsAlive(Character target) throws CharacterAlreadyDeadException{
+        if(!target.isAlive()){
+            throw new CharacterAlreadyDeadException( target.name + " ya está muerto");
+        }
+    }
+
     public int getStrength() {
         return this.strength;
     }
 
     public void performHeavyAttack(Character target) {
-
+        checkTargetNotNull(target);
+        checkIsAlive(target);
         target.receiveDamage(this.strength);
     }
 
     public void enterBerserkMode() {
-        this.status = Status.FURIOUS;
+            checkBerserkMode(this.status, this.name);
+            this.status = Status.FURIOUS;
     }
 }
